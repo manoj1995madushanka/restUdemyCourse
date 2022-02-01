6e616024f95ce582670ee258f04d91fa7256de47
manoj1995madushanka <madushankamanoj414@gmail.com>
2022-02-01 23:00:09 +0530

    create student response class

---
+++ src/main/java/com/example/demo/response/StudentResponse.java
@@ -0,0 +1,10 @@
+package com.example.demo.response;
+
+import lombok.Data;
+
+@Data
+public class StudentResponse {
+    private int id;
+    private String firstName;
+    private String lastName;
+}
