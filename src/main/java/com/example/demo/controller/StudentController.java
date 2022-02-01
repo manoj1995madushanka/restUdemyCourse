c15ca31abefc8cb2e8264beb38102289eeb41c90
manoj1995madushanka <madushankamanoj414@gmail.com>
2022-02-01 22:49:35 +0530

    accessing application property using @Value tag

--- src/main/java/com/example/demo/controller/StudentController.java
+++ src/main/java/com/example/demo/controller/StudentController.java
@@ -1,5 +1,6 @@
 package com.example.demo.controller;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -9,10 +10,14 @@ import org.springframework.web.bind.annotation.RestController;
 public class StudentController {
 
 
+    // accessing properties of application property
+    @Value("${app.name}")
+    private String appName;
+
     @GetMapping("/get")
     //@RequestMapping(value = "/get",method = RequestMethod.GET) getMapping similar to this line also
     public String getStudent()
     {
-        return "student";
+        return appName;
     }
 }
