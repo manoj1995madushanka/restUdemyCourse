d91e9048dd8c831ed47f8544d817d87b356abc2e
manoj1995madushanka <madushankamanoj414@gmail.com>
2022-02-04 16:52:22 +0530

    create repository for student entity

--- src/main/java/com/example/demo/DemoApplication.java
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -3,11 +3,13 @@ package com.example.demo;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 @SpringBootApplication
 // this will scan classes of provided package as a parameter
 //@ComponentScan({"com.example.controller","com.example.service"})
 @EntityScan("com.example.demo.entity")
+@EnableJpaRepositories("com.example.demo.repository")
 public class DemoApplication {
 
 	public static void main(String[] args) {
---
+++ src/main/java/com/example/demo/repository/StudentRepository.java
@@ -0,0 +1,12 @@
+package com.example.demo.repository;
+
+import com.example.demo.entity.Student;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface StudentRepository extends JpaRepository<Student,Integer> {
+
+    // JpaRepository is a combination of CrudRepository<T,ID> and PagingAndSortingRepository<T,ID>
+
+}
