327fcbdff3ba7ab937ac5d83abb1f9a22514d0dd
manoj1995madushanka <madushankamanoj414@gmail.com>
2022-02-04 17:32:36 +0530

    create endpoint for get all students

--- src/main/java/com/example/demo/controller/StudentController.java
+++ src/main/java/com/example/demo/controller/StudentController.java
@@ -1,13 +1,18 @@
 package com.example.demo.controller;
 
+import com.example.demo.entity.Student;
 import com.example.demo.response.StudentResponse;
+import com.example.demo.service.StudentService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.List;
+
 @RestController  // combination of response body and controller
-@RequestMapping("/api/student/") // mapping controller base uri
+@RequestMapping("/api/student") // mapping controller base uri
 public class StudentController {
 
 
@@ -15,11 +20,23 @@ public class StudentController {
     @Value("${app.name:Default value if property not exist}")
     private String appName;
 
+    @Autowired
+    private StudentService studentService;
+
     @GetMapping("/get")
     //@RequestMapping(value = "/get",method = RequestMethod.GET) getMapping similar to this line also
     public StudentResponse getStudent() {
 
         StudentResponse response = new StudentResponse(1, "fName", "lName");
         return response;
+    }
+
+    /**
+     * return all the students db have
+     * */
+    @GetMapping("/getAll")
+    public List<Student> getAll()
+    {
+        return studentService.getStudents();
     }
 }
---
+++ src/main/java/com/example/demo/service/StudentService.java
@@ -0,0 +1,19 @@
+package com.example.demo.service;
+
+import com.example.demo.entity.Student;
+import com.example.demo.repository.StudentRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class StudentService {
+    @Autowired
+    private StudentRepository studentRepository;
+
+    public List<Student> getStudents()
+    {
+        return studentRepository.findAll();
+    }
+}
