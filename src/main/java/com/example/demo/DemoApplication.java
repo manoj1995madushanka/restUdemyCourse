c62e21ea774fdb0c3504da1993cec5cde06c05f6
manoj1995madushanka <madushankamanoj414@gmail.com>
2022-02-06 15:53:49 +0530

    configure swagger

--- src/main/java/com/example/demo/DemoApplication.java
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -4,12 +4,14 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.domain.EntityScan;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 @SpringBootApplication
 // this will scan classes of provided package as a parameter
 //@ComponentScan({"com.example.controller","com.example.service"})
 @EntityScan("com.example.demo.entity")
 @EnableJpaRepositories("com.example.demo.repository")
+@EnableSwagger2 // swagger configuration, for access swagger doc go to localhost:8080/swagger-ui.html
 public class DemoApplication {
 
 	public static void main(String[] args) {
